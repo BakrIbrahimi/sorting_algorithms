a094dc7e6d15ff4c56998e1c6be24691881322d5
Bakr Ibrahimi <bakribra5@gmail.com>
2023-07-23 20:48:20 +0100

    add task 0

--- sort.h
+++ sort.h
@@ -38,6 +38,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* Sorting algorithms */
-
+void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
